be48fa9dddb8a503972017e73589d2ee18680a7e
Siddharth Sharma <ssiddharth408@gmail.com>
2020-09-06 19:03:17 +0530

    virtual assistant

---
+++ ai.py
@@ -0,0 +1,102 @@
+import pyttsx3
+from pyttsx3.drivers import sapi5
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os
+import smtplib
+import random
+from joke.jokes import *
+from joke.quotes import  *
+import winsound
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        print("Good Morning sir")
+        speak("Good Morning sir")
+    elif hour >= 12 and hour < 18:
+        print("Good Afternoon sir")
+        speak("Good Afternoon sir")
+    else:
+        print("Good evening sir")
+        speak("Good evening sir")
+    speak("how may i help you")
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.adjust_for_ambient_noise(source, duration=0.5)
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognising...\n")
+        query = r.recognize_google(audio, language="en-in")
+        print(f"{query}\n")
+    
+    except Exception as e:
+        print("Can you say that again please...")
+        return "None"
+
+    return query
+
+if __name__ == "__main__":
+
+    wishMe()
+    
+    while True:
+        query = takeCommand().lower()
+        
+        if 'wikipedia' in query:
+            speak('Searching...')
+            query = query.replace("Wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+        
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+        
+        elif 'time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M")
+            print(f"Sir the time is {strTime}")
+            speak(f"sir the time is {strTime}")
+            
+        elif 'play music' in query:
+            music_dir = "F:\songs"
+            songs = os.listdir(music_dir)
+            print(f"Playing {songs}")
+            os.startfile(os.path.join(music_dir, songs[0]))
+        
+        elif 'search' in query:
+            google_query = query.replace("search", "")
+            webbrowser.open(f"https://www.google.com/search?q={google_query}")
+            print("Opening google")
+        
+        elif 'joke' or 'jokes' in query:
+            random_joke = random.choice([geek, icanhazdad, chucknorris, icndb])()
+            print(random_joke)
+            speak(random_joke)
+            winsound.PlaySound('joke.wav', winsound.SND_FILENAME)
+        
+        elif 'quotes' or 'quote' in query:
+            today_quote = stormconsultancy()
+            print(today_quote)
+            speak(today_quote)
+        
+            
